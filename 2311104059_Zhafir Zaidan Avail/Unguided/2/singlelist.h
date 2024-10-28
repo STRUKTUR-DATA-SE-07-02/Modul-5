f40369a0f39ec23a4512c0ec381c89bd50064751
Zhafir Avel <167537812+zhafiravel@users.noreply.github.com>
2024-10-29 01:10:02 +0700

    Add files via upload

---
+++ 2311104059_Zhafir Zaidan Avail/Unguided/2/singlelist.h
@@ -0,0 +1,24 @@
+#ifndef SINGLELIST_H
+#define SINGLELIST_H
+
+typedef int infotype;
+typedef struct ElmtList* address;
+
+struct ElmtList {
+    infotype info;
+    address next;
+};
+
+struct List {
+    address first;
+};
+
+// Function prototypes
+void createList(List& L);
+address alokasi(infotype x);
+void dealokasi(address& P);
+void insertFirst(List& L, address P);
+void printInfo(const List& L);
+address findElm(const List& L, infotype x); // Fungsi baru untuk pencarian elemen
+
+#endif
