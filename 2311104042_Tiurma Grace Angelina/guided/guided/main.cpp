6448b702bb22aff98faedbd337fdff762cbc3834
tiurmagrace <168392593+tiurmagrace@users.noreply.github.com>
2024-10-29 21:14:46 +0700

    Add files via upload

---
+++ 2311104042_Tiurma Grace Angelina/guided/guided/main.cpp
@@ -0,0 +1,123 @@
+#include <iostream>
+
+using namespace std;
+
+// Struktur untuk node dalam linked list
+struct Node {
+    int data;
+    Node* next;
+};
+
+// fungsi untuk menambahkan elemen baru ke awal linkded list
+void insertFirst(Node*& head, Node*& tail, int new_data){
+    Node* new_node = new Node();
+    new_node->data = new_data;
+    new_node->next = head;
+    head = new_node;
+    head = new_node;
+
+    if (tail == nullptr){
+        tail = new_node;
+    }
+}
+
+// fungsi menambahkan elemen baru ke akhir linked list
+void insertLast(Node*& head, Node*& tail, int new_data){
+    Node* new_node = new Node();
+    new_node->data = new_data;
+    new_node->next = nullptr;
+
+    if (head == nullptr){
+        head = new_node;
+        tail = new_node;
+    } else {
+        tail->next = new_node;
+        tail = new_node;
+    }
+}
+
+// fungsi mencari elemen dalam linked list
+int findElement(Node* head, int x){
+    Node* current = head;
+    int index = 0;
+
+    while (current != nullptr){
+        if (current->data == x){
+            return index;
+        }
+        current = current->next;
+        index++;
+    }
+    return -1;
+}
+
+// fungsi menampilkan elemen dalam linked list
+void display(Node* node){
+    while (node != nullptr){
+        cout << node->data << " ";
+        node = node->next;
+    }
+    cout << endl;
+}
+
+// fungsi menghapus elemen dari linked list
+void deleteElement(Node*& head, int x){
+    if (head == nullptr){
+        cout << "Linked List Kosong" << endl;
+        return;
+    }
+
+    if (head->data == x){
+        Node* temp = head;
+        head = head->next;
+        delete temp;
+        return;
+    }
+
+    Node* current = head;
+    while (current->next != nullptr){
+        if (current->next->data == x){
+                 Node* temp = current->next;
+            current->next = current->next->next;
+            delete temp;
+            return;
+        }
+        current = current->next;
+    }
+}
+
+int main(){
+    Node* head = nullptr;
+    Node* tail = nullptr;
+
+    insertFirst(head, tail, 3);
+    insertFirst(head, tail, 5);
+    insertFirst(head, tail, 7);
+
+    insertLast(head, tail, 11);
+    insertLast(head, tail, 14);
+    insertLast(head, tail, 18);
+
+    cout << "Elemen dalam linked list: ";
+    display(head);
+
+    int x;
+    cout << "Masukkan elemen yang ingin dicari: ";
+    cin >> x;
+
+    int result = findElement(head, x);
+
+    if (result == -1)
+        cout << "Elemen tidak ditemukan dalam linked list" << endl;
+    else
+        cout << "Elemen ditemukan pada indeks " << result << endl;
+
+    cout << "Masukkan elemen yang ingin dihapus: ";
+    cin >> x;
+    deleteElement(head, x);
+
+    cout << "Elemen dalam linked list setelah penghapusan: ";
+    display(head);
+
+    return 0;
+}
