8a546241c087918166bf570eb0f2f6a7611d1169
FixSann <168392873+FixSann@users.noreply.github.com>
2024-10-29 21:35:01 +0700

    Add files via upload

---
+++ 2311104064_Muhammad Ikhsan Al Hakim/unguided/singlelist.h
@@ -0,0 +1,28 @@
+#ifndef SINGLELIST_H
+#define SINGLELIST_H
+
+// Definiskan infotype sebagai int dan addres  sebagai pointer ke ElmList
+typedef int infotype;
+typedef struct ElmList *address;
+
+// Definisikan struktur untuk setiap elemen (node)
+struct ElmList {
+    infotype info;  // Menyimpan nilai dari node
+    address next;   // pointer ke node berikutnya 
+};
+
+// Definisikan struktur untuk List
+struct List{
+    address First;  // Menunjuk ke node pertama dalam list
+};
+
+// Deklarasi fungsi dan prosedur
+void createList(List &L);
+address alokasi(infotype x);
+void dealokasi(address &P);
+void printInfo(const List &L);
+void insertFirst(List &L, address P);
+address findElm(const List &L, infotype x);
+int totalInfo(const List &L);
+
+#endif
